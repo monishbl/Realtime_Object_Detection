7e36e96d12f2bb6a880f4a0044854344982be358
monishbl <monishbl159@gmail.com>
2024-06-13 21:58:32 +0530

    sad

--- Yolo/detect.py
+++ Yolo/detect.py
@@ -1,8 +1,27 @@
 import cv2
 import numpy as np
+import os
+import requests
 
+# Define the path to the weights file
+weights_path = "Yolo/yolov4.weights"
+
+# Check if the weights file exists
+if not os.path.exists(weights_path):
+    # Define the URL of the weights file in your Hugging Face repository
+    url = "https://cdn-lfs-us-1.huggingface.co/repos/9f/fe/9ffe91d46cfcedc00334199261c9254a8f99615d8fca52412bfd18ff67b7f632/e8a4f6c62188738d86dc6898d82724ec0964d0eb9d2ae0f0a9d53d65d108d562?response-content-disposition=attachment%3B+filename*%3DUTF-8%27%27yolov4.weights%3B+filename%3D%22yolov4.weights%22%3B&Expires=1718554556&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcxODU1NDU1Nn19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLWxmcy11cy0xLmh1Z2dpbmdmYWNlLmNvL3JlcG9zLzlmL2ZlLzlmZmU5MWQ0NmNmY2VkYzAwMzM0MTk5MjYxYzkyNTRhOGY5OTYxNWQ4ZmNhNTI0MTJiZmQxOGZmNjdiN2Y2MzIvZThhNGY2YzYyMTg4NzM4ZDg2ZGM2ODk4ZDgyNzI0ZWMwOTY0ZDBlYjlkMmFlMGYwYTlkNTNkNjVkMTA4ZDU2Mj9yZXNwb25zZS1jb250ZW50LWRpc3Bvc2l0aW9uPSoifV19&Signature=EY3FliLIe47088a9m1wlxC3Ln80G5ZkIMlhoZaa3Ei87AvVGoqApyBoLRz9UCfVUHXiXDTTAdy3KNYZLbqwcaVgc0ifiR5R7vcLb8CXYSvt9CEmOlqY5OTFHOakxVnCiU%7EWAQ2hGUZgBZtI17QdAZ29LXA368sRMQGyzSLVsJ8LSpHlpDImTkAxCO-7sjlzIbammJnWRW6dUV7mb6ByA7sdrWtZayENpKYkqORRiZCErPgoeaNWSi0jd%7E7XD7pDrQ4t1PrbPt1SVMyVyL624-ERBloQ%7E4wgsUL6set8K4bJJsojU8xuR0q0Jl1DIjgY-liIdGPeMAkmMpXAacQ4zhQ__&Key-Pair-Id=K2FPYV99P2N66Q"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Ensure the request was successful
+    response.raise_for_status()
+
+    # Write the content of the response to a file in the parent directory
+    with open(weights_path, "wb") as f:
+        f.write(response.content)
 # Load YOLOv4 model
-net = cv2.dnn.readNet("Yolo\yolov4.weights", "Yolo\yolov4.cfg")
+net = cv2.dnn.readNet(weights_path, "Yolo\yolov4.cfg")
 
 # Load the class labels YOLOv4 was trained on
 with open("Yolo\coco.names", "r") as f:
